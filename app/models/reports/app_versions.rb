15b0f63e69be4bd1f4ecd07685bb6b34f9f1dae8
Kicha <Kicha@bitbucket.org>
2016-04-22 15:58:20 +0200

    Added version code to app versions report
    Change-Id: I44cd54bc905568995124965aaed2c4a85c2296ad

--- app/models/reports/app_versions.rb
+++ app/models/reports/app_versions.rb
@@ -4,14 +4,15 @@ class Reports::AppVersions
 	end
 
 	def self.generate(user, date_from, date_to, params)
-		columns = ['First Name', 'Last Name', 'Email', 'App Version', 'Last sync date']
+		columns = ['First Name', 'Last Name', 'Email', 'App Version', 'Version Code', 'Last sync date']
 		values = []
 		users = User.where('id IN (' + ManagerDriver.manager_drivers_ids(user.company, user).join(',') + ')')
 		users = users.sort_by { |user| [user.last_name, user.first_name] }
 		users.each do |u|
 			last_sync = u.last_sync
 			last_sync = last_sync.strftime("%d/%m/%Y %H:%M") if !last_sync.nil?
-			values.push([ u.first_name, u.last_name, u.email, u.app_version, last_sync])
+			last_log = ApiLogger.where("user_id = ?", u.id).last
+			values.push([ u.first_name, u.last_name, u.email, u.app_version, last_log.app_version_code, last_sync])
 		end
 
 		return [columns, values]
