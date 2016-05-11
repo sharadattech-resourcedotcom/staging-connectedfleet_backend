573ceee1ae14aeb51ce5fa5bbdf4007aedcdc9b4
Kicha <Kicha@bitbucket.org>
2016-05-11 11:24:02 +0200

    Payroll calculation fix
    Change-Id: I652db1cead7d9429b381575fd23bcc1b17375741

--- app/models/payroll.rb
+++ app/models/payroll.rb
@@ -4,6 +4,7 @@ class Payroll < ActiveRecord::Base
 	def self.calculate_for(date)
 		Payroll.where("DATE(for_date) = DATE(?) AND end_datetime IS NOT NULL", date).each do |p|
 			extra_hours = 0
+			normal_hours = 0
 			start_datetime = p.start_datetime.change(sec: 0)
 			end_datetime = p.end_datetime.change(sec: 0)
 			
@@ -16,7 +17,7 @@ class Payroll < ActiveRecord::Base
 				extra_hours = extra_hours + ((end_datetime - end_datetime.change(hour: 18, min: 0)) / 1.hour)
 				end_datetime = end_datetime.change(hour: 18, min: 0)
 			end
-			normal_hours = ((end_datetime - start_datetime) / 1.hour)
+			normal_hours = ((end_datetime - start_datetime) / 1.hour) if (end_datetime - start_datetime) > 0
 			p.normal_hours = normal_hours
 			p.extra_hours = extra_hours
 			p.standard_payment = normal_hours * p.user.driver_type.hourly_rate if !p.user.driver_type.nil?
